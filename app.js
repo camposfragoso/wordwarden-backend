147fc199e80995bb5513e6be51bf3a0d171e846d
Camille <camposfragoso@proton.me>
2024-03-04 09:40:46 +0100

    models

--- app.js
+++ app.js
@@ -1,20 +1,48 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require("dotenv").config() 
+require("./models/connection") 
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var express = require("express")
+var path = require("path")
+var cookieParser = require("cookie-parser")
+var logger = require("morgan")
 
-var app = express();
+var indexRouter = require("./routes/index")
+var usersRouter = require("./routes/users") 
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+var app = express()
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+const cors = require("cors") 
 
-module.exports = app;
+const corsOptions = {
+  origin: function (origin, callback) {
+    const allowedOrigins = [
+        "http://localhost:3000",
+        "http://localhost:3001",
+        "http://localhost:4000",
+        "http://localhost:4001",
+        "https://www.tablee.app",
+        "http://192.168.0.35:4000",
+        "http://192.168.0.35:4001"
+    ];
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+};
+
+app.use(cors(corsOptions)) 
+
+app.use(logger("dev"))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, "public")))
+
+app.use("/", indexRouter)
+app.use("/users", usersRouter) 
+
+module.exports = app
